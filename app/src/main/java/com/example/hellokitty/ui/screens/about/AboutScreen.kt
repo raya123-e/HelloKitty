03ba11b49318746026f17144a5929f5d98cc6e28
raya123-e <githiiclaire@gmail.com>
2025-05-12 15:38:41 +0300

    yeah!!!

--- app/src/main/java/com/example/hellokitty/ui/screens/about/AboutScreen.kt
+++ app/src/main/java/com/example/hellokitty/ui/screens/about/AboutScreen.kt
@@ -2,13 +2,374 @@ package com.example.hellokitty.ui.screens.about
 
 
 
+import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.hellokitty.R
+import com.example.hellokitty.navigation.ROUT_DASHBOARD
+import com.example.hellokitty.ui.theme.Cyan
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(navController: NavController){
+
+    //Scaffold
+
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    Scaffold(
+        //TopBar
+        topBar = {
+            TopAppBar(
+                title = { Text("Service") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate(ROUT_DASHBOARD) }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Cyan,
+                    titleContentColor = Color.Black,
+                    navigationIconContentColor = Color.White
+                )
+            )
+        },
+
+        //BottomBar
+        bottomBar = {
+            NavigationBar(
+                containerColor = Cyan
+            ) {
+                NavigationBarItem(
+                    icon = { Icon(Icons.Default.Home, contentDescription = "Home", tint = Color.Black) },
+                    label = { Text("Home") },
+                    selected = selectedIndex == 0,
+                    onClick = {
+                        selectedIndex = 0
+                        navController.navigate(ROUT_DASHBOARD)
+                    }
+                )
+                NavigationBarItem(
+                    icon = { Icon(Icons.Default.Favorite, contentDescription = "Favorites", tint = Color.Black) },
+                    label = { Text("Favorites") },
+                    selected = selectedIndex == 1,
+                    onClick = {
+                        selectedIndex = 1
+                        // navController.navigate(ROUT_HOME)
+                    }
+                )
+                NavigationBarItem(
+                    icon = { Icon(Icons.Default.Person, contentDescription = "Profile", tint = Color.Black) },
+                    label = { Text("Profile") },
+                    selected = selectedIndex == 2,
+                    onClick = {
+                        selectedIndex = 2
+                        //  navController.navigate(ROUT_HOME)
+                    }
+                )
+
+            }
+        },
+
+        //FloatingActionButton
+        floatingActionButton = {
+
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+            ) {
+
+
+                //Main Contents of the page
+
+                Spacer(modifier = Modifier.height(20.dp))
+                val mContext = LocalContext.current
+
+                //searchbar
+                var search by remember { mutableStateOf("") }
+                OutlinedTextField(
+                    value = search,
+                    onValueChange = { search = it},
+                    modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+                    leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "") },
+                    placeholder = { Text(text = "Search......") },
+
+                    )
+
+                //end of searchbar
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Image(
+                    painter = painterResource(R.drawable.img_14),
+                    contentDescription = "anime",
+                    modifier = Modifier.fillMaxWidth().height(200.dp),
+                    contentScale = ContentScale.FillWidth
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(
+                    text = " Services Available",
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Italic,
+                    textDecoration = TextDecoration.Underline,
+                    fontFamily = FontFamily.SansSerif,
+
+
+
+
+                    )
+                Spacer(modifier = Modifier.height(20.dp))
+
+
+                //Row
+                Row(modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp)
+
+                ) {
+
+                    Image(
+                        painter = painterResource(R.drawable.img_18),
+                        contentDescription = "",
+                        modifier = Modifier.width(200.dp).height(200.dp).clip(shape = RoundedCornerShape(10.dp)),
+                        contentScale = ContentScale.FillWidth,
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    Column {
+                        Text(
+                            text = "Baddie",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text(
+                            text = "A walking billboard for cool confidence — this hot Korean guy blends effortless style with irresistible charm, making him the perfect face for any bold, trend-setting brand.",
+                            fontSize = 10.sp,
+
+                            )
+
+
+
+                        Button(
+                            onClick = {
+                                val callIntent= Intent(Intent.ACTION_DIAL)
+                                callIntent.data="tel:0720245837".toUri()
+                                mContext.startActivity(callIntent)
+                            },
+                            colors = ButtonDefaults.buttonColors(Cyan),
+                            shape = RoundedCornerShape(10.dp),
+                            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+                        ) {
+                            Text(
+                                text = "Call us",
+                                fontSize = 15.sp,
+                            )
+                        }
+
+                    }
+
+                }
+                //End of Row
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+
+
+                //Row-2
+                Row(modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp)
+                ) {
+
+                    Image(
+                        painter = painterResource(R.drawable.img_19),
+                        contentDescription = "",
+                        modifier = Modifier.width(200.dp).height(200.dp).clip(shape = RoundedCornerShape(10.dp)),
+                        contentScale = ContentScale.FillWidth,
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    Column {
+                        Text(
+                            text = "Nonchalance",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text(
+                            text = "Levi commands attention with stoic precision — humanity’s strongest soldier, his sharp gaze and flawless form embody elite strength, discipline, and the ultimate standard in unstoppable cool." ,
+                            fontSize = 10.sp,
+
+                            )
+
+
+
+                        Button(
+                            onClick = {
+                                val callIntent= Intent(Intent.ACTION_DIAL)
+                                callIntent.data="tel:0117434950".toUri()
+                                mContext.startActivity(callIntent)
+                            },
+                            colors = ButtonDefaults.buttonColors(Cyan),
+                            shape = RoundedCornerShape(10.dp),
+                            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+                        ) {
+                            Text(
+                                text = "Call us",
+                                fontSize = 15.sp,
+                            )
+                        }
+
+                    }
+
+                }
+                //End of Row-2
+
+
+                //Row-2
+                Row(
+                    modifier = Modifier
+                        .padding(start = 20.dp, end = 20.dp)
+
+                ) {
+
+                    Image(
+                        painter = painterResource(R.drawable.img_17),
+                        contentDescription = "",
+                        modifier = Modifier.width(200.dp).height(200.dp).clip(shape = RoundedCornerShape(10.dp)),
+                        contentScale = ContentScale.FillWidth,
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    Column {
+                        Text(
+                            text = "Brooding",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text(
+                            text = " Henry Cavill’s Witcher is all grit, intensity, and quiet power, with a stare that cuts deeper than his silver sword.",
+                            fontSize = 10.sp,
+
+                            )
+
+
+
+                        Button(
+                            onClick = {
+                                val callIntent= Intent(Intent.ACTION_DIAL)
+                                callIntent.data="tel:0117434950".toUri()
+                                mContext.startActivity(callIntent)
+                            },
+                            colors = ButtonDefaults.buttonColors(Cyan),
+                            shape = RoundedCornerShape(10.dp),
+                            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+                        ) {
+                            Text(
+                                text = "Call us",
+                                fontSize = 15.sp,
+                            )
+                        }
+
+                    }
+
+                }
+                //End of Row-2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    )
+
+    //End of scaffold
+
+
 
 
 }
